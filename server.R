2bd1d810be4ecad0b5d646fa097889d2bfa19ad7
AzDjam <AzDjam@users.noreply.github.com>
2018-04-26 16:50:51 -0500

    Add files via upload

---
+++ server.R
@@ -0,0 +1,36 @@
+#
+# This is the server  of a Shiny web application. 
+#
+
+library(shiny)
+
+
+
+shinyServer(function(input, output) {
+ 
+ #get the dataset selected
+  dataset <- reactive({
+   
+   get(input$dataset,"package:datasets",inherits=F)
+    
+ })
+ 
+ #summary of the dataset
+  output$summary <- renderPrint({
+    summary(dataset())
+  })
+  
+  # Str of the dataset
+  output$str <- renderPrint({
+    str(dataset())
+  })
+  
+  
+   #  n first obsevations of the dataset
+  output$view <- renderTable({
+     head(dataset(),n = input$obs)
+  })
+  
+ 
+  
+})
